a8e36f3885cbfff84664ec0546addb4064c8b93b
N. Gregory <csovis@gmail.com>
2024-05-14 20:26:17 +0200

    Simulation has visited array bug

--- 1219.js
+++ 1219.js
@@ -45,45 +45,51 @@ const matrixMaxAmount = (grid, row, col, visited) => {
   let value = 0;
   var left = grid[row][col - 1];
   if (left && left > 0 && !visited[row][col - 1]) {
+    var visCopy = [...visited];
     value = matrixMaxAmount(grid, row, col - 1, visited);
-    directionResults.push(value);
+    directionResults.push({ value, visited: visCopy });
   }
 
   var right = grid[row][col + 1];
   if (right && right > 0 && !visited[row][col + 1]) {
+    var visCopy = [...visited];
     value = matrixMaxAmount(grid, row, col + 1, visited);
-    directionResults.push(value);
+    directionResults.push({ value, visited: visCopy });
   }
 
   if (row - 1 > 0) {
     var up = grid[row - 1][col];
     if (up && up > 0 && !visited[row - 1][col]) {
+      var visCopy = [...visited];
       value = matrixMaxAmount(grid, row - 1, col, visited);
-      directionResults.push(value);
+      directionResults.push({ value, visited: visCopy });
     }
   }
 
   if (row + 1 < grid.length) {
     var down = grid[row + 1][col];
     if (down && down > 0 && !visited[row + 1][col]) {
+      var visCopy = [...visited];
       value = matrixMaxAmount(grid, row + 1, col, visited);
-      directionResults.push(value);
+      directionResults.push({ value, visited: visCopy });
     }
   }
 
-  var max = 0;
+  // azt a visited listat kell tovabb vinni amelyik itt a max keresesben is hasznalva van
+  var max = { value: 0 };
   for (let i = 0; i < directionResults.length; i++) {
     const result = directionResults[i];
-    if (result > max) max = result;
+    if (result.value > max.value) max = result;
   }
 
-  return val + max;
+  visited = max.visited;
+  return val + max.value;
 };
 
 // exp 24
-console.log(getMaximumGold(
-  [[0, 6, 0], [5, 8, 7], [0, 9, 0]]
-));
+// console.log(getMaximumGold(
+//   [[0, 6, 0], [5, 8, 7], [0, 9, 0]]
+// ));
 
 // exp 129
 console.log(getMaximumGold(
